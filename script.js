a8c2df40ae693a0baac164db6b565fe8df17d8ad
Klutnie <lukaszspojnia@gmail.com>
2022-11-15 19:08:04 +0100

    ALE SUPER FUNKCJA

--- script.js
+++ script.js
@@ -7,9 +7,23 @@ function startTime() {
     s = checkTime(s);
     document.getElementById('time').innerHTML =  h + ":" + m + ":" + s;
     setTimeout(startTime, 1000);
-  }
-  
-  function checkTime(i) {
+}
+function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
+}
+var r = document.querySelector(':root');
+let count = 1;
+function zmiana_motywu(){
+  
+  let property = document.getElementById(Theme);
+  if (count === 0) {
+      count = 1;
+      r.style.setProperty('--kolorstornki', 'rgba(0, 0, 0, 0.801)');
   }
+  else {
+      count = 0;
+      // pobawic sie w drugi motyw
+      r.style.setProperty('--kolorstornki', 'red');
+  }
+}
